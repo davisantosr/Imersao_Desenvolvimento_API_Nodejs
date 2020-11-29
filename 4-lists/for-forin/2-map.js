a10a0decf3aa3005f803e952b139cdf2aba8462a
davisantosr <davi-rocha2@hotmail.com>
2020-11-28 23:31:04 -0300

    creating our own Array.map|changing prototype

--- 4-lists/for-forin/2-map.js
+++ 4-lists/for-forin/2-map.js
@@ -1,5 +1,15 @@
 const service = require('./service')
 
+Array.prototype.meuMap = function(callback) {
+  const novoArrayMapeado = []
+  for (let i=0; i<= this.length-1; i++){
+    const resultado = callback(this[i], i)
+    novoArrayMapeado.push(resultado)
+  }
+
+  return novoArrayMapeado
+}
+
 async function main() {
   try {
     const results = await service.obterPessoas(`a`)
@@ -10,7 +20,10 @@ async function main() {
     // const names = results.results.map(pessoa => {
     //   return pessoa.name
     // })
-    const names = results.results.map(pessoa => pessoa.name)
+    // const names = results.results.map(pessoa => pessoa.name)
+    const names = results.results.meuMap((pessoa, indice) => {
+      return `[${indice}] ${pessoa.name}]`
+    }) 
     console.log(names);
   } catch (error) {
     console.error('Ocorreu um erro: ', error)
