6f86c19d93dbe123b2db93e8c5461d82cabffbb5
davisantosr <davi-rocha2@hotmail.com>
2020-11-28 22:22:24 -0300

    Events| Using Promise| one  execution

--- 3-events/index.js
+++ 3-events/index.js
@@ -8,9 +8,30 @@ const meuEmissor = new MeuEmissor()
 
 const nomeEvento = 'usuario:click'
 
-meuEmissor.on(nomeEvento, function(click) {
+meuEmissor.on(nomeEvento, function (click) {
   console.log('Um usuário clicou', click)
 })
 
-meuEmissor.emit(nomeEvento, 'na barra de rolagem')
-meuEmissor.emit(nomeEvento, 'ok')
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+
+// let count = 0;
+// setInterval(() => {
+//   meuEmissor.emit(nomeEvento, 'ok' + (count++))
+
+// }, 1000);
+
+
+// Verificar que na Promise o evento executa apenas uma vez
+const stdin = process.openStdin()
+function main() {
+  return new Promise((resolve, reject) => {
+    stdin.addListener('data', function (value) {
+      // console.log(`Você digitou ${value.toString().trim()}`);
+
+      return resolve(value)
+    })
+  })
+}
+main().then(function(resultado) {
+  console.log('resultado: ', resultado.toString())
+})
