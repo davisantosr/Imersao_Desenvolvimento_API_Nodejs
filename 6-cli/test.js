739b8e4f9c2d3463de315df974554786dac65a79
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 15:39:06 -0300

    CREATE|handle files

--- 6-cli/test.js
+++ 6-cli/test.js
@@ -11,18 +11,18 @@ describe('Suite de manipulação de heróis', () => {
 
   it('deve pesquisar um herói usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
-    const resultado = await database.listar(expected.id)
+    const [resultado] = await database.listar(expected.id)
 
     deepEqual(resultado, expected) 
     // ok: o primeiro parametro somente verifica 
     // true ou false para saber se há valor diferente de null
   })
   
-  // it('deve cadastrar um héroi, usando arquivos', async () => {
+  it('deve cadastrar um héroi, usando arquivos', async () => {
 
-  //   const expected = DEFAULT_ITEM_CADASTRAR
-  //   //
-  //   ok(null, expected)
+    const expected = DEFAULT_ITEM_CADASTRAR
+    //
+    deepEqual(expected, expected)
 
-  // })
+  })
 })
