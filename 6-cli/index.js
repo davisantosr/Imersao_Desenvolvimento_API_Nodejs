69bcabbf2937676ccf9e582305d9707cacb9f84f
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 18:07:43 -0300

    add option|add class Heroi with constructor|add Commander check options

--- 6-cli/index.js
+++ 6-cli/index.js
@@ -1,10 +1,55 @@
 const Commander = require('commander')
+const database = require('./database')
+const Database = require('./database')
+const Heroi = require('./heroi')
 
 async function main() {
   
   Commander
     .version('v1')
+    .option('-n, --nome [value]', "Nome do Heroi")
+    .option('-p, --poder [value]', "Poder do herói")
+    .option('-i, --id [value]', "Id do herói")
+
+    .option('-c, --cadastrar', "Cadastrar um heroi")
+    .option('-l, --listar', "Listar um heroi")
+    .option('-r, --remover [value]', "Remove um heroi pelo id")
+    
     .parse(process.argv)
+
+  const heroi = new Heroi(Commander)
+  try {
+    if(Commander.cadastrar){
+      // console.log(heroi);
+      const resultado = await Database.cadastrar(heroi)
+      if(!resultado) {
+        console.error('Heroi não foi cadastrado')
+        return
+      }
+
+      console.log('Heroi cadastrado com sucesso')
+    }
+
+    if(Commander.listar) {
+      const resultado = await Database.listar()
+      console.log(resultado)
+      return
+    }
+
+    if(Commander.remover) {
+      const resultado = await Database.remover(heroi.id)
+      if(!resultado){
+        console.error('Não foi possível remover o heroi')
+        return
+      }
+
+      console.log('Heroi removido com sucesso')
+    }
+    
+  } catch (error) {
+    console.log('Deu erro', error)
+    
+  }
     
 }
 
