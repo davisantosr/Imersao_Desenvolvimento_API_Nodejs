b983378e16e29e4d83417f9018836caf48e74120
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 19:18:42 -0300

    CLI implementation

--- 6-cli/index.js
+++ 6-cli/index.js
@@ -6,6 +6,7 @@ const Heroi = require('./heroi')
 async function main() {
   
   Commander
+    .help("Teste")
     .version('v1')
     .option('-n, --nome [value]', "Nome do Heroi")
     .option('-p, --poder [value]', "Poder do herói")
@@ -14,13 +15,14 @@ async function main() {
     .option('-c, --cadastrar', "Cadastrar um heroi")
     .option('-l, --listar', "Listar um heroi")
     .option('-r, --remover [value]', "Remove um heroi pelo id")
+    .option('-a, --atualizar [value]', "Atualiza um heroi")
     
     .parse(process.argv)
 
   const heroi = new Heroi(Commander)
   try {
     if(Commander.cadastrar){
-      // console.log(heroi);
+      delete heroi.id
       const resultado = await Database.cadastrar(heroi)
       if(!resultado) {
         console.error('Heroi não foi cadastrado')
@@ -44,6 +46,21 @@ async function main() {
       }
 
       console.log('Heroi removido com sucesso')
+    }
+
+    if(Commander.atualizar) {
+      const idParaAtualizar = parseInt(Commander.atualizar);
+      // Remover todas as chaves que estiverem com undefined | null
+      const dado = JSON.stringify(heroi)
+      const heroiAtualizar = JSON.parse(dado)
+      const resultado = Database.atualizar(idParaAtualizar, heroiAtualizar)
+      if(!resultado) {
+        console.error('Não foi possível atualizar o heroi')
+        return
+      }
+      console.log('Heroi atualizado com sucesso')
+
+
     }
     
   } catch (error) {
